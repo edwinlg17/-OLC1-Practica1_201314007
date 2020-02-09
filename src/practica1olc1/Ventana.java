e1feab8eb64dd431f988ea189ead50400a9ec6e0
edwinlg17 <2247574310101@ingenieria.usac.edu.gt>
2020-02-09 13:36:29 -0600

    var a

--- src/practica1olc1/Ventana.java
+++ src/practica1olc1/Ventana.java
@@ -2,22 +2,37 @@ package practica1olc1;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-public class Ventana extends JFrame{
+public class Ventana extends JFrame {
+
     //////////////// ATRIBUTOS
     private GridBagConstraints gbc;
+
+    private JMenuBar jmbBar;
+    private JMenu jmArc;
+    private JMenuItem jmiAbr;
+    private JMenuItem jmiGua;
+    private JMenuItem jmiGuaCom;
+    private JMenuItem jmiGenXML;
     
+    int a = 0;
+
     //////////////// CONTRUCTOR
-    public Ventana(){
+    public Ventana() {
         formatoFormulario();
         formatoComponentes();
         this.pack();
         this.setSize(800, 600);
         this.setLocationRelativeTo(null);
     }
-    
+
     // Formato del Formulario
     private void formatoFormulario() {
         this.setTitle("Practica 1");
@@ -25,12 +40,54 @@ public class Ventana extends JFrame{
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    
+
     // Formato de los Componentes
     private void formatoComponentes() {
+        //////////////////////////// MENU
+        jmbBar = new JMenuBar();
+
+        jmArc = new JMenu("Archivo");
+
+        jmiAbr = new JMenuItem("Abrir");
+        jmiAbr.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                accionBotonAbrir(e);
+            }
+        });
+        jmArc.add(jmiAbr);
+
+        jmiGua = new JMenuItem("Guardar");
+        jmiGua.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                accionBotonGuardar(e);
+            }
+        });
+        jmArc.add(jmiGua);
         
+        jmiGuaCom = new JMenuItem("Guardar Como");
+        jmiGuaCom.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                accionBotonGuardarComo(e);
+            }
+        });
+        jmArc.add(jmiGuaCom);
+        
+        jmiGenXML = new JMenuItem("Generar XML");
+        jmiGenXML.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                accionBotonGenerarXML(e);
+            }
+        });
+        jmArc.add(jmiGenXML);
+        
+        jmbBar.add(jmArc);
+        this.setJMenuBar(jmbBar);
     }
-    
+
     // Funcion que establece los parametros para el grid bag constraints
     public void establecerGBC(int x, int y, int w, int h, double wx, double wy) {
         gbc = new GridBagConstraints();
@@ -41,7 +98,20 @@ public class Ventana extends JFrame{
         gbc.weightx = wx;
         gbc.weighty = wy;
     }
-    
+
     //////////////// METODOS
+    // METODOS MENU
+    private void accionBotonAbrir(ActionEvent evt) {
+        
+    }
+    private void accionBotonGuardar(ActionEvent evt) {
+        
+    }
+    private void accionBotonGuardarComo(ActionEvent evt) {
+        
+    }
+    private void accionBotonGenerarXML(ActionEvent evt) {
+        
+    }
     
 }
