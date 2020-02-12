09a663c841e296ecbc54024a133992c86377270c
edwinlg17 <2247574310101@ingenieria.usac.edu.gt>
2020-02-11 19:54:11 -0600

    Creo la clase para los token

--- src/practica1olc1/AnalizadorLexico.java
+++ src/practica1olc1/AnalizadorLexico.java
@@ -2,12 +2,51 @@ package practica1olc1;
 
 public class AnalizadorLexico {
     //////////////// ATRIBUTOS
+    private String lex;
+    private String tok;
+    private int fil;
+    private int col;
     
     //////////////// CONSTRUCTOR
     public AnalizadorLexico(){
-        
+        this.lex = "";
+        this.tok = "";
+        this.fil = 0;
+        this.col = 0;
+    }
+    
+    public AnalizadorLexico(String lex, String tok, int fil, int col){
+        this.lex = lex;
+        this.tok = tok;
+        this.fil = fil;
+        this.col = col;
     }
     
     //////////////// METODOS
+    public String obtLex() {
+        return lex;
+    }
+    public String obtTok() {
+        return tok;
+    }
+    public int obtFil() {
+        return fil;
+    }
+    public int obtCol() {
+        return col;
+    }
+
+    public void estLex(String lex) {
+        this.lex = lex;
+    }
+    public void estTok(String tok) {
+        this.tok = tok;
+    }
+    public void estFil(int fil) {
+        this.fil = fil;
+    }
+    public void estCol(int col) {
+        this.col = col;
+    }
     
 }
---
+++ src/practica1olc1/Token.java
@@ -0,0 +1,14 @@
+package practica1olc1;
+
+public class Token {
+    //////////////// ATRIBUTOS
+    
+    
+    //////////////// CONSTRUCTOR
+    public Token(){
+        
+    }
+    
+    //////////////// METODOS
+    
+}
