f7a3a6b5ab3a5e1c5a13a66c22de7746ec998114
edwinlg17 <2247574310101@ingenieria.usac.edu.gt>
2020-02-11 22:51:58 -0600

    Trabajando en numeros

--- src/practica1olc1/AnalizadorLexico.java
+++ src/practica1olc1/AnalizadorLexico.java
@@ -18,56 +18,84 @@ public class AnalizadorLexico {
     public void analizar(String tex) {
         System.out.println("/////////// Inicio Analisis");
         lisTok = new LinkedList<>();
-        int ite = 0, est = 0, fil = 0, col = 0;
+        int ite = 0, est = 0, fil = 1, col = 1;
         String lex = "";
-        
-        
-       
-//        while (ite < tex.length()) {
-//            char car = tex.charAt(ite);
-//            
-//            switch (est) {
-//                // estado 0
-//                case 0:
-//                    if (verLetra(car)) {
-//                        lex += car;
-//                        est = 1;
-//                        ite++;
-//                    } else {
-//                        ite++;
-//                    }
-//                    break;
-//                // estado 1
-//                case 1:
-//                    if (verLetra(car) | verNumero(car) | car == '_') {
-//                        lex += car;
-//                        est = 1;
-//                        ite++;
-//                    }else{
-//                        System.out.println(lex);
-//                        lex = "";
-//                        est = 0;
-//                    }
-//                    break;
-//                // estado 2
-//                case 2:
-//
-//                    break;
-//                // estado 3
-//                case 3:
-//
-//                    break;
-//                // estado 4
-//                case 4:
-//
-//                    break;
-//                // ERROR 5
-//                case 5:
-//
-//                    break;
-//            }
-//        }
-        
+
+        int fl = 0, cl = 0;
+
+        while (ite < tex.length()) {
+            char car = tex.charAt(ite);
+
+            switch (est) {
+                // estado 0
+                case 0:
+                    if (verLetra(car)) {
+                        lex += car;
+                        est = 1;
+                        cl = col;
+                        fl = fil;
+                        col++;
+                    } else if (verNumero(car)) {
+                        lex += car;
+                        est = 2;
+                        cl = col;
+                        fl = fil;
+                        col++;
+                    } else if (car == '"') {
+                        col++;
+                    } else if (car == '\r' | car == '\t') {
+                        // caracteres omitibles
+                    } else if (car == '\n') {
+                        fil++;
+                        col = 1;
+                    } else if (car == ' ') {
+                        col++;
+                    } else {
+                        col++;
+                    }
+                    ite++;
+                    break;
+                // estado 1
+                case 1:
+                    if (verLetra(car) | verNumero(car) | car == '_') {
+                        lex += car;
+                        est = 1;
+                        ite++;
+                        col++;
+                    } else {
+                        System.out.println(lex + " - " + fl + " - " + cl);
+                        lex = "";
+                        est = 0;
+                    }
+                    break;
+                // estado 2
+                case 2:
+                    if (verNumero(car)) {
+                        lex += car;
+                        est = 2;
+                        ite++;
+                        col++;
+                    }else{
+                        System.out.println(lex + " - " + fl + " - " + cl);
+                        lex = "";
+                        est = 0;
+                    }
+                    break;
+                // estado 3
+                case 3:
+
+                    break;
+                // estado 4
+                case 4:
+
+                    break;
+                // ERROR 5
+                case 5:
+
+                    break;
+            }
+        }
+
         System.out.println("/////////// Fin Analisis");
         //return lisTok;
     }
