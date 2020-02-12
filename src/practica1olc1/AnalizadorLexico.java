77cff2377722b4b8c925f8b71710d7bda770af24
edwinlg17 <2247574310101@ingenieria.usac.edu.gt>
2020-02-11 19:37:58 -0600

    Inicio Analizador Lexico

---
+++ src/practica1olc1/AnalizadorLexico.java
@@ -0,0 +1,13 @@
+package practica1olc1;
+
+public class AnalizadorLexico {
+    //////////////// ATRIBUTOS
+    
+    //////////////// CONSTRUCTOR
+    public AnalizadorLexico(){
+        
+    }
+    
+    //////////////// METODOS
+    
+}
